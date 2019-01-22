4079662e415abb4c9d3a4811234a0bbd8a386864
Esteban Aguirre Patiño <esteban.aguirre9212@gmail.com>
2019-01-22 11:31:53 -0500

    13. Esperas Explicitas-fix in product added

--- src/page/product-added-modal.page.ts
+++ src/page/product-added-modal.page.ts
@@ -1,10 +1,11 @@
-import { $, ElementFinder } from 'protractor';
+import { $, ElementFinder, browser, ExpectedConditions  } from 'protractor';
 export class ProductAddedModalPage {
   private proceedToCheckoutButton: ElementFinder;
   constructor () {
     this.proceedToCheckoutButton = $('[style*="display: block;"] .button-container > a');
   }
   public async goToCheckoutButton(): Promise <void> {
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.proceedToCheckoutButton), 5000);
     await this.proceedToCheckoutButton.click();
   }
 }
